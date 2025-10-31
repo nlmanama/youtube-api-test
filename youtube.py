b29e5b449eb9ca7cafd4aa73c5ca2f146b179f13
nlmanama <nlminhanh12@gmail.com>
2025-10-31 12:20:13 -0700

    refactored into questions

--- youtube.py
+++ youtube.py
@@ -50,6 +50,8 @@ def _():
 
 
 @app.function
+# Helper function 1
+
 def request_videos(part, chart, amount):
     request = youtube.videos().list(
         part=part,
@@ -62,58 +64,68 @@ def request_videos(part, chart, amount):
 
 @app.cell
 def _():
+    # Testing code - should probably be removed or comment out in student code version
+
     # Making the request for the statistics of 20 most viral videos
     # Contains things like viewCount, likeCount etc
     statistics_response = request_videos("statistics", "mostPopular", 20)
 
     # Making the request for the contentDetails of 20 most viral videos
-    # Contains things like video length
+    # Contains things like video duration
     content_response = request_videos("contentDetails", "mostPopular", 20)
     # Resopnse object to be passed into functions to use
     return content_response, statistics_response
 
 
 @app.function
-def save_graph(videos, axis, graph_name):
-    # Parsing response
-    # Response JSON format shown here: https://developers.google.com/youtube/v3/docs/videos/list
-    # Video format: https://developers.google.com/youtube/v3/docs/videos#resource
-    view_count = []
-    axis_count = []
+# Helper function 2
 
-    for video in videos:
-        view_count.append(video["statistics"]["viewCount"])
-        axis_count.append(video["statistics"][axis])
-
-    # Plotting a chart
-
-    fig, ax = plt.subplots()
-
-    ax.scatter(view_count, axis_count, alpha=0.6)
-
-    # Slightly hacky solution to make the x-axis cleaner
-    # Found here: https://stackoverflow.com/questions/54783160/x-axis-tick-labels-are-too-dense-when-drawing-plots
-    ax.set_xticks(ax.get_xticks()[::3])
-
-
-    ax.set_xlabel("View Count")
-    ax.set_ylabel(axis)
-    ax.set_title("Scatter Plot of View Count to " + axis)
-
+def save_graph_png(plt, graph_name):
     plt.savefig(graph_name, dpi=300, bbox_inches='tight')
     print(f"Plot saved as '{graph_name}'")
     plt.show()
 
-    return 0
+
+@app.function
+# Question 1 - easiest
+
+def video_length_historgram(response):
+    # Extract video durations
+    durations = []
+    for item in response["items"]:
+        durations.append(item["contentDetails"]["duration"])
+
+    # Convert ISO 8601 durations to seconds
+    seconds = []
+    for duration in durations:
+        total_seconds = isodate.parse_duration(duration).total_seconds()
+        seconds.append(total_seconds)
+
+    # Create a DataFrame for easier handling
+    df = pd.DataFrame(seconds, columns=["Video Length (seconds)"])
+
+    # Plot histogram
+    plt.figure(figsize=(8, 5))
+    plt.hist(df["Video Length (seconds)"], bins=10, edgecolor="black")
+    plt.title("Distribution of YouTube Video Lengths")
+    plt.xlabel("Length (seconds)")
+    plt.ylabel("Number of Videos")
+    plt.grid(True, alpha=0.3)
+    plt.show()
+    save_graph_png(plt, "duration.png")
 
 
 @app.cell
-def _(statistics_response):
-    save_graph(statistics_response["items"], "likeCount", "like_scatter_plot.png")
+def _(content_response):
+    # Testing code - should probably be removed or comment out in student code version
+
+    video_length_historgram(content_response)
     return
 
 
 @app.function
+# Question 2 part 1 (parsing)
+
 def get_video_dislikes(response, r, n):
     """
     Return the n most disliked videos from the 30 most popular videos.
@@ -147,42 +159,45 @@ def get_video_dislikes(response, r, n):
     return videos[:n]
 
 
+@app.function
+# Question 2 part 2 - plotting (or we can just give code as it and not make it a question)
+
+def dislike_count_scatter_plot(videos):
+    # Parsing response
+    # Response JSON format shown here: https://developers.google.com/youtube/v3/docs/videos/list
+    # Video format: https://developers.google.com/youtube/v3/docs/videos#resource
+    view_count = []
+    dislike_count = []
+
+    for video in videos:
+        view_count.append(video["statistics"]["viewCount"])
+        dislike_count.append(video["statistics"]["dislikeCount"])
+
+    # Plotting a chart
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(view_count, dislike_count, alpha=0.6)
+
+    # Slightly hacky solution to make the x-axis cleaner
+    # Found here: https://stackoverflow.com/questions/54783160/x-axis-tick-labels-are-too-dense-when-drawing-plots
+    ax.set_xticks(ax.get_xticks()[::3])
+
+
+    ax.set_xlabel("View Count")
+    ax.set_ylabel("Dislike Count")
+    ax.set_title("Scatter Plot of View Count to Dislike Count")
+
+    save_graph_png(fig, "dislikeCount.png")
+    return plt.gca()
+
+
 @app.cell
 def _(statistics_response):
-    save_graph(get_video_dislikes(statistics_response, 0.86, 30), "dislikeCount", "dislike_scatter_plot_1.png")
-    save_graph(get_video_dislikes(statistics_response, 0.5, 15), "dislikeCount", "dislike_scatter_plot_2.png")
-    return
+    # Testing code - should probably be removed or comment out in student code version
 
-
-@app.function
-def video_length_historgram(response):
-    # Extract video durations
-    durations = []
-    for item in response["items"]:
-        durations.append(item["contentDetails"]["duration"])
-    
-    # Convert ISO 8601 durations to seconds
-    seconds = []
-    for duration in durations:
-        total_seconds = isodate.parse_duration(duration).total_seconds()
-        seconds.append(total_seconds)
-    
-    # Create a DataFrame for easier handling
-    df = pd.DataFrame(seconds, columns=["Video Length (seconds)"])
-    
-    # Plot histogram
-    plt.figure(figsize=(8, 5))
-    plt.hist(df["Video Length (seconds)"], bins=10, edgecolor="black")
-    plt.title("Distribution of YouTube Video Lengths")
-    plt.xlabel("Length (seconds)")
-    plt.ylabel("Number of Videos")
-    plt.grid(True, alpha=0.3)
-    plt.show()
-
-
-@app.cell
-def _(content_response):
-    video_length_historgram(content_response)
+    dislike_count_scatter_plot(get_video_dislikes(statistics_response, 0.86, 30))
+    dislike_count_scatter_plot(get_video_dislikes(statistics_response, 0.5, 15))
     return
 
 
